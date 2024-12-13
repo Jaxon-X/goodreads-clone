e2e65fcf7e875781d43aa8c41299e8fa135007b1
Jaxon <xudoyberdiyevjahon8@gmail.com>
2024-12-13 16:24:27 +0500

    add registerForm and write some unit tests for user

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,55 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import reverse
+
+
+class RegistrationTestCase(TestCase):
+    def test_user_account_is_created(self):
+        self.client.post(
+                        reverse('users:register'),
+                         data = {
+                             "username": "jaxxon",
+                             "first_name": "Jakhon",
+                             "last_name":"Xudoyberdiyev",
+                             "email":"xudoyberdiyev33@gmail.com",
+                             "password":"somepassword",
+                         })
+
+        user = User.objects.get(username="jaxxon")
+
+        self.assertEqual(user.first_name, "Jakhon")
+        self.assertEqual(user.last_name, "Xudoyberdiyev")
+        self.assertEqual(user.email, "xudoyberdiyev33@gmail.com")
+        self.assertTrue(user.check_password("somepassword")),
+
+    def test_user_required_fields(self):
+        response = self.client.post(
+            reverse('users:register'),
+            data = {
+                "first_name":'Jakhon',
+                "last_name":"Xudoyberdiyev",
+            }
+        )
+
+        user_count = User.objects.count()
+        self.assertEqual(user_count, 0)
+        self.assertEquals(response.context['form'].errors['username'], ['This field is required.'])
+        self.assertEquals(response.context['form'].errors['password'], ['This field is required.'])
+
+    def test_invalid_email(self):
+        response = self.client.post(
+            reverse('users:register'),
+            data = {
+                "first_name":'Jakhon',
+                "last_name":"Xudoyberdiyev",
+                "email":"xudoyberdiyev",
+            }
+        )
+
+        user_count = User.objects.count()
+        self.assertEqual(user_count, 0)
+        self.assertEquals(response.context['form'].errors['email'], ['Enter a valid email address.'])
+
+
+    def test
--- users/views.py
+++ users/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth.models import User
+from users.forms import UserCreateForm
+
 
 class RegisterView(View):
     def get(self, request):
-        return render(request, 'users/register.html')
+
+        form = UserCreateForm()
+        context = {'form': form}
+        return render(request, 'users/register.html', context)
+
+    def post(self, request):
+        form = UserCreateForm(data = request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('users:login')
+        else:
+            context = {'form': form}
+            return render(request, 'users/register.html', context)
 
 
 
