abe6250edf20f842fca4ff20e433c7429cb751d0
Jaxon <xudoyberdiyevjahon8@gmail.com>
2024-12-23 01:26:07 +0500

    add search place

--- books/tests.py
+++ books/tests.py
@@ -6,22 +6,22 @@ from books.models import Book
 
 class BooksTestCase(TestCase):
     def test_no_books(self):
-        repsonse = self.client.get(reverse('books:list'))
+        response = self.client.get(reverse('books:list'))
 
-        self.assertContains(repsonse, 'No books found.')
+        self.assertContains(response, 'No books found.')
 
     def test_books_list(self):
         book1 = Book.objects.create(title='Book 1', description='desc1', isbn='11223')
         book2 = Book.objects.create(title='Book 2', description='desc2', isbn='343242')
         book3 = Book.objects.create(title='Book 3', description='desc3', isbn='242455')
 
-        response = self.client.get(reverse('books:list'))
-        # books = Book.objects.all()
+        response = self.client.get(reverse('books:list') + "?page_size=2")
         for book in [book1, book2]:
             self.assertContains(response, book.title)
 
-        response = self.client.get(reverse('books:list') + "?page=2")
-        self.assertContains(response, book3.title)
+        self.assertNotContains(response, book3.title)
+        response2 = self.client.get(reverse('books:list') + "?page=2&page_size=2")
+        self.assertContains(response2, book3.title)
 
 
     def test_book_detail(self):
@@ -29,4 +29,22 @@ class BooksTestCase(TestCase):
         response = self.client.get(reverse('books:detail', kwargs={'book_id': book.id}))
         self.assertContains(response, book.title)
         self.assertContains(response, book.description)
-        print(response)
+
+    def test_search_book(self):
+        book1 = Book.objects.create(title='sport', description='desc1', isbn='11223')
+        book2 = Book.objects.create(title='car', description='desc2', isbn='343242')
+        book3 = Book.objects.create(title='Adabiyot', description='desc3', isbn='242455')
+
+        response = self.client.get(reverse('books:list') + "?q=sport")
+        self.assertContains(response, book1.title)
+        self.assertNotContains(response, book2.title)
+        self.assertNotContains(response, book3.title)
+        response = self.client.get(reverse('books:list') + "?q=car")
+        self.assertContains(response, book2.title)
+        self.assertNotContains(response, book1.title)
+        self.assertNotContains(response, book3.title)
+        response = self.client.get(reverse('books:list') + "?q=Adabiyot")
+        self.assertContains(response, book3.title)
+        self.assertNotContains(response, book2.title)
+        self.assertNotContains(response, book1.title)
+
--- books/views.py
+++ books/views.py
@@ -5,27 +5,31 @@ from django.shortcuts import render
 from django.views import View
 from books.models import  Book
 
-class BooksView(ListView):
-    template_name = 'books/books_list.html'
-    queryset = Book.objects.all()
-    context_object_name = 'books'
-    paginate_by = 2
+# class BooksView(ListView):
+#     template_name = 'books/books_list.html'
+#     queryset = Book.objects.all()
+#     context_object_name = 'books'
+#     paginate_by = 2
 
-# class BooksView(View):
-#
-#     def get(self, request):
-#
-#         books = Book.objects.all().order_by('id')
-#         page_size = request.GET.get('page_size' ,2)
-#         paginator = Paginator(books,page_size)
-#
-#         page_num = request.GET.get('page',1)
-#         print(page_num)
-#         page_obj = paginator.get_page(page_num)
-#         print(page_obj.object_list)
-#         context = {'page_obj': page_obj}
-#
-#         return render(request, 'books/books_list.html', context)
+class BooksView(View):
+
+    def get(self, request):
+
+        books = Book.objects.all().order_by('id')
+        search_query = request.GET.get('q', "")
+        if search_query:
+            books = Book.objects.filter(title__icontains=search_query)
+        page_size = request.GET.get('page_size' ,2)
+        paginator = Paginator(books,page_size)
+
+        page_num = request.GET.get('page',1)
+        page_obj = paginator.get_page(page_num)
+
+        return render(
+            request,
+            'books/books_list.html',
+            {'page_obj': page_obj, "search_query":search_query}
+        )
 
 
 
